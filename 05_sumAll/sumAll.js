b626ef224d74d46b35a1084e6dc928366397c8e9
Josh <bricsio.joshua.benavides@gmail.com>
2022-03-04 22:18:05 +1000

    Completed my solutions for javascript exercises 04, 04, 05, 06, and 07.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+    if (typeof int1 != "number" || typeof int2 != "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    let nextNum = int1 < int2 ? int1 : int2;
+    let lastNum = int1 >= int2 ? int1 : int2;
+    while (nextNum <= lastNum) {
+        sum += nextNum;
+        nextNum++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // ([°F] − 32) × 5⁄9
+  // console.log(Math.round(((temp - 32) * 5 / 9) * 10) / 10);
+  return Math.round(((temp - 32) * 5 / 9) * 10) / 10;
+  
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // [°C] × 9⁄5 + 32 
+  // console.log(Math.round((temp * 9 / 5 + 32) * 10) / 10);
+  return Math.round((temp * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
